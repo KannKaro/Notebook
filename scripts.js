519507b9221520c9e9ee82cb7bbb632cbe3421ac
KannKaro <111279140+KannKaro@users.noreply.github.com>
2023-09-19 20:57:23 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+
+var FontStyle = function (font) {
+    document.getElementById('Container').style.fontFamily = font.value; //zamiana rodzaju czcionki
+}
+var FontSize = function (font) {
+    document.getElementById('Container').style.fontSize = font.value; //zmiana wielkosci czcionki
+}
