7830c5a35140bcf3f12ea45d572ddf004202e608
KannKaro <111279140+KannKaro@users.noreply.github.com>
2023-09-22 18:58:15 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,3 +5,51 @@ var FontStyle = function (font) {
 var FontSize = function (font) {
     document.getElementById('Container').style.fontSize = font.value; //zmiana wielkosci czcionki
 }
+
+const saveFile = async () => {
+    try {
+        var mystring = document.getElementById('Container').value
+        var myblob = new Blob([mystring], {
+            type: 'text/plain'
+        });
+        const handle = await window.showSaveFilePicker({
+            types: [{
+                accept: {
+                    // Omitted
+                },
+            }],
+        });
+        const writable = await handle.createWritable();
+        await writable.write(myblob);
+        await writable.close();
+        return handle;
+    } catch (err) {
+        console.error(err.name, err.message);
+    }
+};
+
+let input = document.querySelector('input')
+
+let container = document.querySelector('textarea')
+
+input.addEventListener('change', () => {
+    let files = input.files;
+ 
+    if (files.length == 0) return;
+
+    const file = files[0];
+
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+        const file = e.target.result;
+ 
+        const lines = file.split(/\r\n|\n/);
+        container.value = lines.join('\n');
+ 
+    };
+    
+    reader.onerror = (e) => alert(e.target.error.name);
+ 
+    reader.readAsText(file);
+});
